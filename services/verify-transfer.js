354a3346f3ac9d8c91ff18eff039073cee84e9c0
Precious Damisa <preciousdamisa@gmail.com>
2020-07-04 14:40:24 +0100

    Update

--- services/verify-transfer.js
+++ services/verify-transfer.js
@@ -1,29 +1,29 @@
-const config = require("config");
+const config = require('config');
 
-const http = require("./http");
+const http = require('./http');
 
 const url =
-  "https://ravesandboxapi.flutterwave.com/flwv3-pug/getpaidx/api/v2/verify";
+  'https://ravesandboxapi.flutterwave.com/flwv3-pug/getpaidx/api/v2/verify';
 
 async function verifyTransfer(data) {
   const res = await http.post(url, {
-    SECKEY: config.get('flutterwaveSecretKey'),
+    SECKEY: 'FLWSECK-26b84e92406f072fb7c6527b758daea2-X',
     txref: data.txref,
   });
 
   if (
-    res.data.data.status === "successful" &&
+    res.data.data.status === 'successful' &&
     res.data.data.chargecode === '00'
   ) {
     if (res.data.data.amount >= data.amount) {
       return {
-        status: "successful",
+        status: 'successful',
         code: 0,
         chargeAmount: res.data.data.amount,
       };
     } else {
       return {
-        status: "not successful",
+        status: 'not successful',
         code: 1,
         chargeAmount: res.data.data.amount,
       };
